c840c5dc54299e78be1eb16c633ac60d28e313d1
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-08-12 18:33:23 -0300

    feat: export button lista de opções

--- src/components/Buttons/Export.stories.js
+++ src/components/Buttons/Export.stories.js
@@ -1,13 +1,20 @@
 import Export from './Export.vue';
 
 export default {
-    title: 'Example/Buttons/Export',
-    component: Export,
+	title: 'Example/Buttons/Export',
+	component: Export,
 };
 
 export const Primary = () => ({
-    components: { Export },
-    template: '<Export />',
+	components: { Export },
+	template: '<Export />',
+});
+
+export const List = () => ({
+	components: { Export },
+	template: `<Export :optionsList="[{ key: 1, label: 'teste'}]" />`,
 });
 
 Primary.storyName = 'Básico';
+
+List.storyName = 'Lista';
--- src/components/Buttons/Export.vue
+++ src/components/Buttons/Export.vue
@@ -1,30 +1,84 @@
 <template>
-    <v-btn @click="onClick" dense class="v-btn--responsive" outlined title="Exportar">
-        <v-icon>mdi-file-export-outline</v-icon>
-        Exportar
-    </v-btn>
+	<v-btn
+		v-if="optionsList.length == 0"
+		@click="onClick"
+		dense
+		class="v-btn--responsive"
+		outlined
+		title="Exportar"
+	>
+		<v-icon class="mr-2">mdi-file-export-outline</v-icon>
+		Exportar
+	</v-btn>
+	<v-menu v-else v-model="togglePopover">
+		<template v-slot:activator="{ on, attrs }">
+			<v-btn
+				v-bind="attrs"
+				v-on="on"
+				dense
+				class="v-btn--responsive"
+				outlined
+				title="Exportar"
+				@onClick="togglePopover = true"
+			>
+				Exportar
+				<v-icon class="ml-2 mr-0">
+					{{ togglePopover ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+				</v-icon>
+			</v-btn>
+		</template>
+
+		<v-list dense class="pa-0 mt-9">
+			<v-list-item
+				v-for="item in optionsList"
+				:key="item.key"
+				link
+				:title="item.label"
+				@click="onClick(item.key)"
+			>
+				{{ item.label }}
+			</v-list-item>
+		</v-list>
+	</v-menu>
 </template>
 <script>
 import VBtn from 'vuetify/lib/components/VBtn';
 import VIcon from 'vuetify/lib/components/VIcon';
+import VList from 'vuetify/lib/components/VList/VList';
+import VMenu from 'vuetify/lib/components/VMenu';
+import VListItem from 'vuetify/lib/components/VList/VListItem';
 export default {
-    name: 'ExportButton',
-    components: {
-        VBtn,
-        VIcon,
-    },
-    methods: {
-        onClick() {
-            this.$emit('onClick');
-        },
-    },
+	name: 'ExportButton',
+	props: {
+		optionsList: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	data() {
+		return {
+			togglePopover: false,
+		};
+	},
+	components: {
+		VBtn,
+		VIcon,
+		VList,
+		VListItem,
+		VMenu,
+	},
+	methods: {
+		onClick(key) {
+			this.$emit('onClick', key);
+		},
+	},
 };
 </script>
 <style scoped lang="scss">
 .v-btn {
-    color: var(--v-extra-lighten2);
-    .v-icon {
-        margin-right: 1rem;
-    }
+	color: var(--v-extra-lighten2);
+}
+.v-list-item--link {
+	font-size: 0.875rem;
 }
 </style>
