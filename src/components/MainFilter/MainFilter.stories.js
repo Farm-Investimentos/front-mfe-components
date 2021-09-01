32fc0ca093d417e4c3a8613e052793221736de9e
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-09-01 09:28:06 -0300

    feat: ajuste no emit de evento do input e css

--- src/components/MainFilter/MainFilter.stories.js
+++ src/components/MainFilter/MainFilter.stories.js
@@ -1,13 +1,25 @@
 import MainFilter from './MainFilter.vue';
 
 export default {
-    title: 'Example/MainFilter',
-    component: MainFilter,
+	title: 'Example/MainFilter',
+	component: MainFilter,
 };
 
 export const Primary = () => ({
-    components: { MainFilter },
-    template: '<MainFilter />',
+	components: { MainFilter },
+	template: '<MainFilter />',
+});
+
+export const Secondary = () => ({
+	components: { MainFilter },
+	template: '<MainFilter :hasExtraFilters="false" />',
+});
+
+export const CustomLabel = () => ({
+	components: { MainFilter },
+	template: '<MainFilter label="Custom" />',
 });
 
 Primary.storyName = 'Básico';
+Secondary.storyName = 'Apenas input';
+CustomLabel.storyName = 'Custom Label';
