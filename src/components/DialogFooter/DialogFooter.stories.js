c3c1ba61e6e6ad7789188d6aed833dfedf28892a
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-12-04 09:40:19 -0300

    feat: ícone no botão confirmar do DialogFooter

--- src/components/DialogFooter/DialogFooter.stories.js
+++ src/components/DialogFooter/DialogFooter.stories.js
@@ -1,19 +1,28 @@
 import DialogFooter from './DialogFooter.vue';
 
 export default {
-    title: 'Example/Dialog/DialogFooter',
-    component: DialogFooter,
+	title: 'Example/Dialog/DialogFooter',
+	component: DialogFooter,
 };
 
 export const Primary = () => ({
-    components: { DialogFooter },
-    template: '<DialogFooter />',
+	components: { DialogFooter },
+	template: '<DialogFooter />',
 });
 
 export const Secondary = () => ({
-    components: { DialogFooter },
-    template: `<DialogFooter :extraButtons="[{ label: 'Random label', color: 'error', outlined: true }]" />`,
+	components: { DialogFooter },
+	template: `<DialogFooter :extraButtons="[{ label: 'Random label', color: 'error', outlined: true }]" />`,
+});
+
+export const ConfirmIcon = () => ({
+	components: { DialogFooter },
+	template: `<div><DialogFooter confirmIcon="check" /><br />
+	<DialogFooter confirmIcon="check" confirmColor="yellow" /><br />
+	<DialogFooter confirmIcon="check" confirmColor="accent" /></div>
+	`,
 });
 
 Primary.storyName = 'Básico';
 Secondary.storyName = 'Botão extra';
+ConfirmIcon.storyName = 'Ícone confirmar';
