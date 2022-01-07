d504836ecd4f0be7bbe4ef66e9991876b70347a2
Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
2022-01-07 18:11:11 -0300

    fix: first column TH width if is data-table-select (#63)

--- src/components/DataTableHeader/__tests__/DataTableHeader.spec.js
+++ src/components/DataTableHeader/__tests__/DataTableHeader.spec.js
@@ -3,13 +3,15 @@ import DataTableHeader from '../DataTableHeader';
 
 describe('DataTableHeader component', () => {
 	let wrapper;
+	let component;
 
 	beforeEach(() => {
 		wrapper = shallowMount(DataTableHeader, {
 			propsData: {
 				headers: [],
-			}
+			},
 		});
+		component = wrapper.vm;
 	});
 
 	test('Created hook', () => {
@@ -19,6 +21,19 @@ describe('DataTableHeader component', () => {
 	describe('mount component', () => {
 		it('renders correctly', () => {
 			expect(wrapper.element).toMatchSnapshot();
+		});
+	});
+
+	describe('Methods', () => {
+		it('Should check if item is data-table-select column', () => {
+			expect(component.isTHDataTableSelect({ value: 'data-table-select' })).toBeTruthy();
+			expect(component.isTHDataTableSelect({ value: 'anything else' })).toBeFalsy();
+		});
+
+		it('Should get th width', () => {
+			expect(component.thWidth({ value: 'data-table-select' })).toEqual('64px');
+			expect(component.thWidth({ width: 24 })).toEqual('24px');
+			expect(component.thWidth({})).toEqual('auto');
 		});
 	});
 });
