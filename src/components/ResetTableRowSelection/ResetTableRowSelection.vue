cb27415adb0d5955e15d3cd8fe28f0aed5b6033e
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-01-25 11:34:22 -0300

    refactor: reset table row component

--- src/components/ResetTableRowSelection/ResetTableRowSelection.vue
+++ src/components/ResetTableRowSelection/ResetTableRowSelection.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="ml-6 mr-3 d-flex align-center">
-		Total de linhas selecionadas: {{ inputVal.length }}
-		<v-btn color="error" @click="reset" small dense class="ml-3" v-if="inputVal.length > 0">
+		Total de linhas selecionadas: {{ length }}
+		<v-btn color="error" @click="reset" small dense class="ml-3" v-if="length > 0">
 			<v-icon small> mdi-trash-can </v-icon>
 			Limpar
 		</v-btn>
@@ -12,34 +12,23 @@ import Vue from 'vue';
 import VBtn from 'vuetify/lib/components/VBtn';
 import VIcon from 'vuetify/lib/components/VIcon';
 export default Vue.extend({
-	name: 'farm-tablerowselection',
+	// name: 'farm-tablerowselection',
 	components: {
 		VBtn,
 		VIcon,
 	},
 	props: {
 		/**
-		 * Variável usada como v-model
-		 * contém a lista selecionada
+		 * Current items length selected
 		 */
-		value: {
-			required: true,
-			type: Array,
-		},
-	},
-	computed: {
-		inputVal: {
-			get() {
-				return this.value;
-			},
-			set(val) {
-				this.$emit('input', val);
-			},
+		length: {
+			default: 0,
+			type: Number,
 		},
 	},
 	methods: {
 		reset() {
-			this.inputVal = [];
+			this.$emit('onReset');
 		},
 	},
 });
