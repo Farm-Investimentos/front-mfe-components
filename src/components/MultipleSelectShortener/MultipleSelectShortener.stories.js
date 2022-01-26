faadcaed22d0af2dc9c4c05d61ccc07fa8d7d2cf
viniciuse-secari-farm <84734874+viniciuse-secari-farm@users.noreply.github.com>
2022-01-26 16:26:13 -0300

    feat: multiple select shortener component added (#88)
    * feat: multiple select shortener component added
    
    * chore: build
    
    * chore: new minor version
    
    * fix: .stories archive added
    
    * Update MultipleSelectShortener.vue
    
    * fix: lint

---
+++ src/components/MultipleSelectShortener/MultipleSelectShortener.stories.js
@@ -0,0 +1,37 @@
+import MultipleSelectShortener from './MultipleSelectShortener';
+
+export default {
+	title: 'Example/MultipleSelectShortener',
+	component: MultipleSelectShortener,
+};
+
+export const Primary = () => ({
+	components: { MultipleSelectShortener },
+	data() {
+		return {
+			items: {
+				id: 0,
+				label: `Convidar`,
+			},
+		};
+	},
+	template: '<MultipleSelectShortener :item="items" :index="0" :itemsLength="1"/>',
+});
+
+export const Secondary = () => ({
+	components: { MultipleSelectShortener },
+	data() {
+		return {
+			items: [
+				{
+					id: 0,
+					label: `Convidar`
+				},
+			],
+		};
+	},
+	template: '<MultipleSelectShortener :item="{...items}" :index="1" :itemsLength="3"/>',
+});
+
+Primary.storyName = 'Básico';
+Secondary.storyName = 'Multiplos';
