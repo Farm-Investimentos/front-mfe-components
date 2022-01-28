526f074927cafd8ffb144793e0ca541631e2261a
vinicius secari <vinicius.secari@farminvestimentos.com.br>
2022-01-28 09:29:18 -0300

    fix: text size updated

--- src/components/MultipleSelectShortener/MultipleSelectShortener.vue
+++ src/components/MultipleSelectShortener/MultipleSelectShortener.vue
@@ -1,11 +1,9 @@
 <template>
 	<div class="align-center">
-		<div>
-			<span class="mr-1" v-if="index === 0">{{ item[labelKey] }}</span>
-			<span v-if="index === 1">
-				(+{{ itemsLength - 1 }} {{ itemsLength > 2 ? 'outros' : 'outro' }} )
-			</span>
-		</div>
+		<span class="mr-1 label" v-if="index === 0">{{ item[labelKey] }}</span>
+		<span class="label" v-if="index === 1">
+			(+{{ itemsLength - 1 }} {{ itemsLength > 2 ? 'outros' : 'outro' }} )
+		</span>
 	</div>
 </template>
 
@@ -14,19 +12,37 @@ import Vue from 'vue';
 export default Vue.extend({
 	name: 'farm-multiple-select-shortener',
 	props: {
+		/**
+		 * Key used to get value from item
+		 */
 		labelKey: {
 			type: String,
 			default: 'label',
 		},
+		/**
+		 * Item from v-select
+		 */
 		item: {
 			type: Object,
 		},
+		/**
+		 * Index from slot
+		 */
 		index: {
 			type: Number,
 		},
+		/**
+		 * Items count
+		 */
 		itemsLength: {
 			type: Number,
 		},
 	},
 });
 </script>
+
+<style scoped>
+.label {
+	font-size: small;
+}
+</style>
