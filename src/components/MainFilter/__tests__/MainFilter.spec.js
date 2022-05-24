2538d4f5f48b5b511c34decdec896c4e0853918c
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-05-24 08:57:49 -0300

    feat: main filter better UX filter button

--- src/components/MainFilter/__tests__/MainFilter.spec.js
+++ src/components/MainFilter/__tests__/MainFilter.spec.js
@@ -20,4 +20,22 @@ describe('MainFilter component', () => {
 			expect(wrapper.emitted().onClick).toBeTruthy();
 		});
 	});
+
+	describe('computed properties', () => {
+		it('get extraFiltersBtnLabel', async () => {
+			expect(component.extraFiltersBtnLabel).toEqual('Ver filtros');
+			await wrapper.setProps({
+				showFilters: true,
+			});
+			expect(component.extraFiltersBtnLabel).not.toEqual('Ver filtros');
+		});
+
+		it('get extraFiltersBtnIcon', async () => {
+			expect(component.extraFiltersBtnIcon).toEqual('mdi-filter');
+			await wrapper.setProps({
+				showFilters: true,
+			});
+			expect(component.extraFiltersBtnIcon).toEqual('mdi-filter-off');
+		});
+	});
 });
