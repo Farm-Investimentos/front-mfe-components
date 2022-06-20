2570ad56a6d7923f48a92f09c9438de0115016ca
viniciussecari <vinicius.secari@farminvestimentos.com.br>
2022-06-20 15:22:56 -0300

    fat: RadioButton Component included

---
+++ src/components/RadioButton/RadioButton.vue
@@ -0,0 +1,61 @@
+<template>
+	<v-radio-group v-model="selectedVal" :column="isBlock">
+		<v-radio
+			:class="`${isBlock ? '' : 'ml-3 mr-3'}`"
+			v-for="(n, i) in labels"
+			:key="i"
+			:label="n"
+			color="secondary"
+		>
+		</v-radio>
+	</v-radio-group>
+</template>
+
+<script>
+import Vue from 'vue';
+import { VRadioGroup, VRadio } from 'vuetify/lib/components/VRadioGroup';
+
+export default Vue.extend({
+	name: 'farm-radio-button',
+	components: {
+		VRadioGroup,
+		VRadio,
+	},
+	props: {
+		/**
+		 * value to change with v-model
+		 *  */
+		value: {
+			require: true,
+		},
+		/**
+		 * labels to show radio buttons aside
+		 */
+		labels: {
+			type: Array,
+			require: true,
+		},
+		/**
+		 * to change if is vertical or horizontal show
+		 */
+		isBlock: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	computed: {
+		selectedVal: {
+			get() {
+				return this.value;
+			},
+			set(val) {
+				this.$emit('input', val);
+			},
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+@import './RadioButton.scss';
+</style>
---
+++ src/components/RadioButton/__tests__/RadioButton.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from '@vue/test-utils';
+import RadioButton from '../RadioButton.vue';
+
+describe('RadioButton component', () => {
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = shallowMount(RadioButton, {
+			propsData: {
+				value: 2,
+				label: ['teste', 'teste', 'teste', 'teste'],
+				isBlock: true,
+			},
+		});
+	});
+
+	test('Created hook', () => {
+		expect(wrapper).toBeDefined();
+	});
+});
