63b8eb486b785761b85c8b58fe9e5577ca451650
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-06-27 16:01:09 -0300

    refactor: scss external file

--- src/components/Buttons/DefaultButton/DefaultButton.vue
+++ src/components/Buttons/DefaultButton/DefaultButton.vue
@@ -1,5 +1,9 @@
 <template>
-	<button v-on="$listeners" :type="type" :class="classes"><slot></slot></button>
+	<button v-on="$listeners" :type="type" :class="classes">
+		<span class="farm-btn__content">
+			<slot></slot>
+		</span>
+	</button>
 </template>
 <script lang="ts">
 import Vue from 'vue';
@@ -47,66 +51,5 @@ export default Vue.extend({
 });
 </script>
 <style lang="scss" scoped>
-$colors: primary, secondary, error, extra, gray, accent, yellow;
-
-.farm-btn {
-	font-size: 0.75rem;
-	align-items: center;
-	border-radius: 4px;
-	display: inline-flex;
-	flex: 0 0 auto;
-	font-weight: 500;
-	justify-content: center;
-	outline: 0;
-	position: relative;
-	text-decoration: none;
-	transition-duration: 0.28s;
-	transition-property: box-shadow, transform, opacity;
-	transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-	user-select: none;
-	vertical-align: middle;
-	white-space: nowrap;
-	line-height: 1rem;
-	height: 36px;
-	min-width: 64px;
-	padding: 0 16px;
-}
-
-@each $color in $colors {
-	#{'.farm-btn--' + $color} {
-		background-color: var(--v-#{$color}-base);
-		color: white;
-
-		&:before {
-			background-color: currentColor;
-			border-radius: inherit;
-			bottom: 0;
-			color: inherit;
-			content: '';
-			left: 0;
-			opacity: 0;
-			pointer-events: none;
-			position: absolute;
-			right: 0;
-			top: 0;
-		}
-
-		&:hover {
-			&:before {
-				opacity: 0.08;
-			}
-		}
-	}
-	.farm-btn--outlined#{'.farm-btn--' + $color} {
-		border: 1px solid var(--v-#{$color}-base);
-		background: white;
-		color: var(--v-#{$color}-base);
-	}
-
-	.farm-btn--plain#{'.farm-btn--' + $color} {
-		border: none;
-		background: white;
-		color: var(--v-#{$color}-base);
-	}
-}
+@import 'DefaultButton.scss';
 </style>
