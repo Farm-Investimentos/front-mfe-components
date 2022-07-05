bc0353a5f1b3d6414506ef2ced0f4c87c4cfbdea
Dirceu Rezende <dirceu.rezende@farminvestimentos.com.br>
2022-07-05 17:48:45 -0300

    feat: add background-color to divider from Logger

--- src/components/Logger/Logger.vue
+++ src/components/Logger/Logger.vue
@@ -2,7 +2,19 @@
 	<section class="logger">
 		<template v-for="(item, index) in items">
 			<LoggerItem :item="item" :key="index" />
-			<div v-if="hasDivider(index)" class="logger__divider" :key="'divider_' + index"></div>
+			<div
+				:class="{
+					'logger__divider--success': isPreviousLogAndCurrentLogSuccess(index),
+					'logger__divider--error': isPreviousLogAndCurrentLogError(index),
+					'logger__divider--success-to-error':
+						isPreviousLogErrorAndCurrentLogSuccess(index),
+					'logger__divider--error-to-success':
+						isPreviousLogSuccessAndCurrentLogError(index),
+				}"
+				v-if="hasDivider(index)"
+				class="logger__divider"
+				:key="'divider_' + index"
+			></div>
 		</template>
 	</section>
 </template>
@@ -23,6 +35,24 @@ export default Vue.extend({
 	methods: {
 		hasDivider(index: number): boolean {
 			return index < this.items.length - 1;
+		},
+		isPreviousLogAndCurrentLogError(index: number): boolean {
+			return this.isError(index) && this.isError(index + 1);
+		},
+		isPreviousLogAndCurrentLogSuccess(index: number): boolean {
+			return this.isSuccess(index) && this.isSuccess(index + 1);
+		},
+		isPreviousLogErrorAndCurrentLogSuccess(index: number): boolean {
+			return this.isSuccess(index) && this.isError(index + 1);
+		},
+		isPreviousLogSuccessAndCurrentLogError(index: number): boolean {
+			return this.isError(index) && this.isSuccess(index + 1);
+		},
+		isError(index: number): boolean {
+			return this.items[index].status === 'error';
+		},
+		isSuccess(index: number): boolean {
+			return this.items[index].status === 'success';
 		},
 	},
 });
