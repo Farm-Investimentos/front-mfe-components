71e645e011723114991bf78fa09c9d02efeb4b37
Dirceu-Souza-Rezende <96076023+Dirceu-Souza-Rezende@users.noreply.github.com>
2022-07-05 18:14:31 -0300

    feat: add background-color to divider from Logger (#247)

--- src/components/Logger/__tests__/Logger.spec.js
+++ src/components/Logger/__tests__/Logger.spec.js
@@ -3,6 +3,7 @@ import Logger from '../Logger';
 
 describe('Logger component', () => {
 	let wrapper;
+	let component;
 
 	beforeEach(() => {
 		wrapper = shallowMount(Logger, {
@@ -15,12 +16,107 @@ describe('Logger component', () => {
 						formattedDate: '13/06/2022  20:40',
 						status: 'error',
 					},
+					{
+						icon: 'book',
+						message: 'Recusado entre as pré elegíveis',
+						userName: 'Cleyton Rasta',
+						formattedDate: '13/06/2022  20:40',
+						status: 'error',
+					},
+					{
+						icon: 'book',
+						message: 'Aceito entre as pré elegíveis',
+						userName: 'Cleyton Rasta',
+						formattedDate: '13/06/2022  20:40',
+						status: 'success',
+					},
+					{
+						icon: 'book',
+						message: 'Aceito entre as pré elegíveis',
+						userName: 'Cleyton Rasta',
+						formattedDate: '13/06/2022  20:40',
+						status: 'success',
+					},
+					{
+						icon: 'book',
+						message: 'Recusado entre as pré elegíveis',
+						userName: 'Cleyton Rasta',
+						formattedDate: '13/06/2022  20:40',
+						status: 'error',
+					},
 				],
 			},
 		});
+		component = wrapper.vm;
 	});
 
 	test('Logger created', () => {
 		expect(wrapper).toBeDefined();
+	});
+
+	describe('Methods', () => {
+		describe('hasDivider', () => {
+			it('should return true when it has a next item', () => {
+				expect(component.hasDivider(0)).toBeTruthy();
+			});
+
+			it('should return false when it has not a next item', () => {
+				expect(component.hasDivider(4)).toBeFalsy();
+			});
+		});
+
+		describe('isPreviousLogAndCurrentLogError', () => {
+			it('should return true', () => {
+				expect(component.isPreviousLogAndCurrentLogError(0)).toBeTruthy();
+			});
+
+			it('should return false', () => {
+				expect(component.isPreviousLogAndCurrentLogError(1)).toBeFalsy();
+			});
+		});
+
+		describe('isPreviousLogAndCurrentLogSuccess', () => {
+			it('should return true', () => {
+				expect(component.isPreviousLogAndCurrentLogSuccess(2)).toBeTruthy();
+			});
+			it('should return false', () => {
+				expect(component.isPreviousLogAndCurrentLogSuccess(0)).toBeFalsy();
+			});
+		});
+
+		describe('isPreviousLogErrorAndCurrentLogSuccess', () => {
+			it('should return true', () => {
+				expect(component.isPreviousLogErrorAndCurrentLogSuccess(3)).toBeTruthy();
+			});
+			it('should return false', () => {
+				expect(component.isPreviousLogErrorAndCurrentLogSuccess(2)).toBeFalsy();
+			});
+		});
+		describe('isPreviousLogSuccessAndCurrentLogError', () => {
+			it('should return true', () => {
+				expect(component.isPreviousLogSuccessAndCurrentLogError(1)).toBeTruthy();
+			});
+			it('should false', () => {
+				expect(component.isPreviousLogSuccessAndCurrentLogError(2)).toBeFalsy();
+			});
+		});
+
+		describe('isError', () => {
+			it('should true when the status is error', () => {
+				expect(component.isError(1)).toBeTruthy();
+			});
+			it('should false when the status is success', () => {
+				expect(component.isError(2)).toBeFalsy();
+			});
+		});
+
+		describe('isSuccess', () => {
+			it('should return true when the status is success', () => {
+				expect(component.isSuccess(2)).toBeTruthy();
+			});
+			it('should false when the status is error', () => {
+				expect(component.isSuccess(1)).toBeFalsy();
+			});
+		});
 	});
 });
