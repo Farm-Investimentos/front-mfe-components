6ca3965565607c84992df074693c5d866649949e
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-07-12 19:30:54 -0300

    fix: import VTextField

--- src/components/TextField/TextField.vue
+++ src/components/TextField/TextField.vue
@@ -1,12 +1,16 @@
 <template>
-	<v-text-field class="farm-textfield" v-on="{ ...$listeners }" v-bind="$attrs" outlined /> 
+	<v-text-field class="farm-textfield" v-on="{ ...$listeners }" v-bind="$attrs" outlined />
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import { VTextField } from 'vuetify/lib/components/VTextField';
 
 export default Vue.extend({
 	name: 'farm-textfield',
 	inheritAttrs: true,
+	components: {
+		VTextField,
+	},
 });
 </script>
 <style lang="scss" scoped>
