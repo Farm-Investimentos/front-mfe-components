bf925f589939ea96f2bd703602855de95d4aaf16
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-07-13 08:30:10 -0300

    chore: better docs and pipeline

--- src/components/TextField/TextField.stories.js
+++ src/components/TextField/TextField.stories.js
@@ -1,8 +1,24 @@
+import { withDesign } from 'storybook-addon-designs';
 import TextField from './TextField.vue';
 
 export default {
 	title: 'API/Form/TextField',
 	component: TextField,
+	decorators: [withDesign],
+	parameters: {
+		docs: {
+			description: {
+				component: `Text field<br />
+				selector: <em>farm-texfield</em>
+				`,
+			},
+		},
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/1f84J4m1IBghWhozQvdyyt/%E2%9C%85---Design-System-%7C-v1?node-id=1503%3A227',
+		},
+		viewMode: 'docs',
+	},
 };
 
 export const Primary = () => ({
@@ -18,4 +34,14 @@ export const Primary = () => ({
 	</div>`,
 });
 
-Primary.storyName = 'Basic';
+const parameters = {
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/1f84J4m1IBghWhozQvdyyt/%E2%9C%85---Design-System-%7C-v1?node-id=1503%3A227',
+	},
+};
+
+Primary.story = {
+	name: 'Basic',
+	parameters,
+};
