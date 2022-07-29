9de3004f37804657be7b0284da86bc7be7b080e8
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-07-29 19:19:25 +0100

    feat: IconBox colors example

--- .storybook/vuetify_storybook.js
+++ .storybook/vuetify_storybook.js
@@ -1,59 +1,15 @@
 // .storybook/vuetify_storybook.js
 import Vue from 'vue';
-import Vuetify from 'vuetify'; // loads all components
-import 'vuetify/dist/vuetify.min.css'; // all the css for components
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import VuetifyDialog from 'vuetify-dialog';
-//import config from '../src/plugins/vuetifyConfig'; // basic config with theme
 
 import VueMask from 'v-mask';
 
 Vue.use(VueMask);
 
-const config = {
-	theme: {
-		themes: {
-			light: {
-				primary: '#1C1C1C',
-				secondary: {
-					base: '#00B493',
-					lighten5: '#EBFFFB',
-					lighten4: '#D6FFF8',
-					lighten3: '#ADFFF0',
-					lighten2: '#99FFEC',
-					lighten1: '#70FFE5',
-					darken1: '#47FFDD',
-					darken2: '#0AFFD2',
-					darken3: '#00F5C8',
-					darken4: '#00CCA7',
-				},
-				error: '#EA5455',
-				extra: {
-					base: '#FFFFFF',
-					lighten1: '#467D7E',
-					lighten2: '#1A3738',
-					lighten3: '#0B1A1B',
-					lighten4: '#FADD9E',
-					lighten5: '#F8CC6B',
-				},
-				gray: {
-					base: '#5C5C5C',
-					lighten1: '#858585',
-					lighten2: '#D6D6D6',
-					lighten3: '#0B1A1B',
-					lighten4: '#F5F5F5',
-					lighten5: '#FFFFFF',
-				},
-				accent: {
-					base: '#5089DE',
-				},
-				yellow: '#F8CC6B',
-			},
-		},
-		options: {
-			customProperties: true,
-		},
-	},
-};
+import config from '@farm-investimentos/front-mfe-libs-ts/consts/vuetify';
+
 const vuetify = new Vuetify(config);
 Vue.use(VuetifyDialog, {
 	context: {
---
+++ src/configurations/colors.ts
@@ -0,0 +1,13 @@
+export default [
+	'primary',
+	'secondary',
+	'error',
+	'extra',
+	'accent',
+	'info',
+	'success',
+	'warning',
+	'gray',
+	'yellow',
+	'white',
+];
