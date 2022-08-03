5d409cc40dd00be1575f2a7c4bab21b34b14a51f
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-08-03 13:23:06 +0100

    feat: size prop for iconbox & fix width/height

--- src/components/IconBox/__tests__/IconBox.spec.js
+++ src/components/IconBox/__tests__/IconBox.spec.js
@@ -3,13 +3,15 @@ import IconBox from '../IconBox';
 
 describe('IconBox component', () => {
 	let wrapper;
+	let component;
 
 	beforeEach(() => {
 		wrapper = shallowMount(IconBox, {
 			propsData: {
 				icon: 'mdi-book',
-			}
+			},
 		});
+		component = wrapper.vm;
 	});
 
 	test('Created hook', () => {
@@ -19,6 +21,17 @@ describe('IconBox component', () => {
 	describe('mount component', () => {
 		it('renders correctly', () => {
 			expect(wrapper.element).toMatchSnapshot();
+		});
+	});
+
+	describe('computed properties', () => {
+		it('get iconParsed', () => {
+			expect(component.iconParsed).toBeDefined();
+			expect(component.iconParsed).toEqual('book');
+		});
+
+		it('get cssColorClass', () => {
+			expect(component.cssColorClass).toEqual('farm-icon-box--secondary');
 		});
 	});
 });
