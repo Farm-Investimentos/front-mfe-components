b84d74c74b6bf6bccd20b26dadea61c2e9e62a5d
Kainan Gabriel <kainanytbr@gmail.com>
2022-08-04 13:16:13 -0300

    feat: typography limit valide tags

--- src/components/Typography/Typography.vue
+++ src/components/Typography/Typography.vue
@@ -14,23 +14,14 @@
 import Vue, { computed, PropType, ref } from 'vue';
 
 const breakPoints = ['xs', 'sm', 'md', 'lg', 'xl'];
+const valideHtmlTags = ['p', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'legend', 'label', 'li'];
 
 export default Vue.extend({
 	inheritAttrs: true,
 	props: {
 		tag: {
 			type: String as PropType<
-				| 'p'
-				| 'span'
-				| 'h1'
-				| 'h2'
-				| 'h3'
-				| 'h4'
-				| 'h5'
-				| 'h6'
-				| 'legend'
-				| 'label'
-				| 'li'
+				'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'legend' | 'label' | 'li'
 			>,
 			default: 'p',
 		},
@@ -40,9 +31,17 @@ export default Vue.extend({
 	},
 	setup(props, context) {
 		let style = ref({});
+		let tag = ref({});
 
 		const { weight } = context.attrs;
 		const { size } = props;
+
+		const receivedTag = props.tag;
+		const defaultHtmlTag = 'p';
+
+		const isHtmlTagValide = computed(() => valideHtmlTags.includes(receivedTag));
+
+		!isHtmlTagValide.value ? (tag.value = defaultHtmlTag) : (tag.value = receivedTag);
 
 		const isSizeFromBreakpoints = computed(() => breakPoints.includes(size));
 
@@ -53,10 +52,10 @@ export default Vue.extend({
 		if (weight) {
 			style.value.fontWeight = weight;
 		}
-
 		return {
 			style,
 			isSizeFromBreakpoints,
+			tag,
 		};
 	},
 });
