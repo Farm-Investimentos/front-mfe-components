38474725d122eee815c6d3e78e7495ec71c8f697
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-08-05 14:23:39 +0100

    refactor: BEM style [skip ci]

--- src/components/AlertReload/AlertReload.vue
+++ src/components/AlertReload/AlertReload.vue
@@ -7,11 +7,11 @@
 	>
 		<div>
 			<farm-icon color="error">alert</farm-icon>
-			<farm-typography tag="span" color="error" class="farm-alert-reload--label">
+			<farm-typography tag="span" color="error" class="farm-alert-reload__label">
 				{{ label }}
 			</farm-typography>
 		</div>
-		<farm-btn color="secondary" class="ml-3" alt="Recarregar" @click="$emit('onClick')">
+		<farm-btn color="secondary" alt="Recarregar" @click="$emit('onClick')">
 			<farm-icon>refresh</farm-icon>
 			Recarregar
 		</farm-btn>
