386e2610525d11b07211d774b9684501b4f04b2c
Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
2022-08-09 11:06:44 -0300

    feat: STOR-35 (#350)
    * feat: STOR-35
    
    * test: specs for STOR-35
    
    * refactor: prop to ref
    
    * refactor: removed sample code [skip ci]

---
+++ src/components/Typography/Heading/configurations.ts
@@ -0,0 +1,10 @@
+const Keys = {
+	1: { weight: 300, size: '60px' },
+	2: { weight: 300, size: '56px' },
+	3: { weight: 400, size: '48px' },
+	4: { weight: 400, size: '32px' },
+	5: { weight: 400, size: 'xl' },
+	6: { weight: 500, size: 'lg' },
+};
+
+export { Keys };
