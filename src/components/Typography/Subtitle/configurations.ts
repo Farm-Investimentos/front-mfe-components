368d9995908fd73de34dee0fa86afb16f235a42e
Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
2022-08-09 15:28:06 -0300

    feat: subtitle typography component (#354)
    * feat: subtitle typography component
    
    * refactor: spec name

---
+++ src/components/Typography/Subtitle/configurations.ts
@@ -0,0 +1,8 @@
+const Keys = {
+	'1_regular': { weight: 400, size: 'default' },
+	'1_medium': { weight: 500, size: 'default' },
+	'2_regular': { weight: 400, size: 'md' },
+	'2_medium': { weight: 500, size: 'md' },
+};
+
+export { Keys };
