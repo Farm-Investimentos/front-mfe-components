62b974fdcc25322f349ab9b487ffdd24a7b694d8
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-08-11 13:19:26 +0100

    feat: examples STOR-37

---
+++ src/components/Typography/BodyText/BodyText.vue
@@ -0,0 +1,53 @@
+<template>
+	<farm-typography
+		:weight="weight"
+		:size="size"
+		:key="key"
+		:class="{ [`farm-subtitle--${type}`]: true, [`farm-subtitle--${variation}`]: true }"
+		v-bind="$attrs"
+	>
+		<slot></slot>
+	</farm-typography>
+</template>
+<script lang="ts">
+import Vue, { computed, ref, watch, toRefs, PropType } from 'vue';
+import Typography from '../Typography.vue';
+
+import { Keys } from './configurations';
+
+export default Vue.extend({
+	inheritAttrs: true,
+	name: 'farm-bodytext',
+	props: {
+		/**
+		 * Type of the bodytext
+		 */
+		type: { type: Number as PropType<1 | 2 | 3>, default: 1 },
+		/**
+		 * Variation of the bodytext
+		 */
+		variation: { type: String as PropType<'regular' | 'medium' | 'bold'>, default: 'medium' },
+	},
+	setup(props) {
+		const { type, variation } = toRefs(props);
+		const key = ref(type.value + '_' + variation.value);
+
+		watch(
+			() => props,
+			() => {
+				key.value = type.value + '_' + variation.value;
+			}
+		);
+
+		const config = computed(() => Keys[key.value]);
+		const weight = computed(() => config.value.weight);
+		const size = computed(() => config.value.size);
+
+		return { weight, size, key, type, variation };
+	},
+	components: { 'farm-typography': Typography },
+});
+</script>
+<style lang="scss" scoped>
+@import 'BodyText';
+</style>
