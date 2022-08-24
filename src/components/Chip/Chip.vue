e93b65dbade4ef409a0240d7ee853d948c64b2df
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-08-24 18:17:09 +0100

    refactor: Chip

--- src/components/Chip/Chip.vue
+++ src/components/Chip/Chip.vue
@@ -1,21 +1,16 @@
 <template>
 	<span :class="{ 'farm-chip': true }">
-        <farm-typography tag="span" size="sm">
-            <slot></slot>
-        </farm-typography>
+		<farm-typography tag="span" size="sm">
+			<slot></slot>
+		</farm-typography>
 	</span>
 </template>
 <script lang="ts">
 import Vue from 'vue';
-import Typography from '../Typography';
 
 export default Vue.extend({
 	name: 'farm-chip',
-    inheritAttrs: true,
-	setup() {},
-	components: {
-		'farm-typography': Typography,
-	},
+	inheritAttrs: true,
 });
 </script>
 <style lang="scss" scoped>
