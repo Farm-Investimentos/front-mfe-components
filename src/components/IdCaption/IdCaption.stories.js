79901b713d29c0fc74ce609b95f804fbd33ea3d2
Kainan Gabriel <kainanytbr@gmail.com>
2022-08-29 17:57:18 -0300

    feat: IdCaption component spec & stories variation

--- src/components/IdCaption/IdCaption.stories.js
+++ src/components/IdCaption/IdCaption.stories.js
@@ -10,7 +10,88 @@ export const Primary = () => ({
     <farm-idcaption 
     icon="account-box-outline" 
     link="/admin/dashboard" 
-    copyText="texto a copiar">
+    copyText="texto a copiar"
+    >
+        <template v-slot:title="{}">
+            Upper Line Text
+        </template>
+        <template v-slot:subtitle="{}">
+            Lower: Line Text
+        </template>
+    </farm-idcaption>
+    `,
+});
+
+export const Linkless = () => ({
+	template: `
+    <farm-idcaption 
+    icon="account-box-outline"
+    copyText="texto a copiar"
+    >
+        <template v-slot:title="{}">
+            Upper Line Text
+        </template>
+        <template v-slot:subtitle="{}">
+            Lower: Line Text
+        </template>
+    </farm-idcaption>
+    
+    `,
+});
+
+export const NoUpperText = () => ({
+	template: `
+    <farm-idcaption 
+    icon="account-box-outline"
+    copyText="texto a copiar"
+    link="/admin/dashboard" 
+
+    >
+        <template v-slot:subtitle="{}">
+            Lower: Line Text
+        </template>
+    </farm-idcaption>
+    
+    `,
+});
+
+export const NoBottomText = () => ({
+	template: `
+    <farm-idcaption 
+    icon="account-box-outline" 
+    link="/admin/dashboard" 
+    copyText="texto a copiar"
+    >
+        <template v-slot:title="{}">
+            Upper Line Text
+        </template>
+       
+    </farm-idcaption>
+    `,
+});
+
+export const Iconless = () => ({
+	template: `
+    <farm-idcaption 
+    link="/admin/dashboard" 
+    copyText="texto a copiar"
+    >
+        <template v-slot:title="{}">
+            Upper Line Text
+        </template>
+        <template v-slot:subtitle="{}">
+            Lower: Line Text
+        </template>
+    </farm-idcaption>
+    `,
+});
+
+export const NoTextToClip = () => ({
+	template: `
+    <farm-idcaption 
+    icon="account-box-outline" 
+    link="/admin/dashboard" 
+    >
         <template v-slot:title="{}">
             Upper Line Text
         </template>
