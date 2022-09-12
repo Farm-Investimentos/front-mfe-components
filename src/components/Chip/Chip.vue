39bfc7cd020b16a06616529277495c8d37daaa5d
Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
2022-09-12 11:06:01 -0300

    feat: STOR-56 (#451)
    * Pulling current version from develop v9.2.1 to main (#447)
    
    * feat: filter icon STOR-84 (#444)
    
    * feat: STOR-81 (#445)
    
    * feat: add details to LoggerItem
    
    * test: add new unit tests
    
    * fix: lint errors
    
    * refactor: add title to button
    
    * fix: data table paginator footer margin
    
    * fix: data table paginator footer margin
    
    * Update package.json - new version [skip ci] (#446)
    
    Co-authored-by: christian-benseler-farm <christian-benseler-farm@users.noreply.github.com>
    
    Co-authored-by: Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
    Co-authored-by: Dirceu-Souza-Rezende <96076023+Dirceu-Souza-Rezende@users.noreply.github.com>
    Co-authored-by: Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: christian-benseler-farm <christian-benseler-farm@users.noreply.github.com>
    
    * Update package.json
    
    * feat: STOR-56
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dirceu-Souza-Rezende <96076023+Dirceu-Souza-Rezende@users.noreply.github.com>
    Co-authored-by: christian-benseler-farm <christian-benseler-farm@users.noreply.github.com>

--- src/components/Chip/Chip.vue
+++ src/components/Chip/Chip.vue
@@ -1,8 +1,6 @@
 <template>
-	<span :class="{ 'farm-chip': true }">
-		<farm-typography tag="span" size="sm">
-			<slot></slot>
-		</farm-typography>
+	<span :class="{ 'farm-chip': true, 'farm-chip--dense': dense }">
+		<farm-typography tag="span" size="sm"> <slot></slot> </farm-typography>
 	</span>
 </template>
 <script lang="ts">
@@ -11,6 +9,15 @@ import Vue from 'vue';
 export default Vue.extend({
 	name: 'farm-chip',
 	inheritAttrs: true,
+	props: {
+		/**
+		 * Is dense (not 100% width)?
+		 */
+		dense: {
+			type: Boolean,
+			default: false,
+		},
+	},
 });
 </script>
 <style lang="scss" scoped>
