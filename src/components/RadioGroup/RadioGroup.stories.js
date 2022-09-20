430898d3e06296c6fb7485304148f7ed5d10c77c
Wesley de Santana <wesleysantana_gabriel@outlook.com>
2022-09-20 14:18:11 -0300

    feat: farm-radio-group structure

---
+++ src/components/RadioGroup/RadioGroup.stories.js
@@ -0,0 +1,24 @@
+import RadioGroup from './RadioGroup';
+
+export default {
+	title: 'Form/RadioGroup',
+	component: RadioGroup,
+};
+
+export const Primary = () => ({
+	components: { 'farm-radio-group': RadioGroup },
+	data() {
+		return {
+			buttons: [
+                { label: 'Button 1', id: 1 }, 
+                { label: 'Button 2', id: 2 }, 
+                { label: 'Button 3', id: 3 }
+            ],
+		};
+	},
+	template: `<div>
+	    <farm-radio-group v-model="buttons" />
+	</div>`,
+});
+
+Primary.storyName = 'Basic';
---
+++ src/components/RadioGroup/RadioGroup.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div 
+            v-for="(button, index) in buttons" 
+            :key="`farm-radio-group_` + index"
+        >
+            <input type="radio" :id="`farm-radio-group_` + index" :name="button.label" value="30">
+            <label :for="`farm-radio-group_` + index">
+                {{button.label}}
+            </label>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import IRadioGroup from './IRadioGroup';
+export default Vue.extend({
+	name: 'farm-radio-group',
+	props: {
+		/**
+		 * Array of buttons
+		 */
+		buttons: {
+			type: Array as PropType<Array<IRadioGroup>>,
+			default: () => [],
+		},
+	},
+
+});
+</script>
+<style lang="scss" scoped>
+@import './RadioGroup.scss';
+</style>
