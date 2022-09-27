a4af14884d488f121360711749cef8ce48c719b8
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-09-27 13:17:47 +0100

    refactor: remove v-icon from TableContextKenu STOR-67

--- src/components/TableContextMenu/TableContextMenu.stories.js
+++ src/components/TableContextMenu/TableContextMenu.stories.js
@@ -10,16 +10,31 @@ export default {
 			type: 'figma',
 			url: 'https://www.figma.com/file/rkkAsX4IP0tzv1udIDXlqe/%E2%9C%8D---Onboarding---PJ?node-id=3516%3A11516',
 		},
+		docs: {
+			description: {
+				component: `TableContextMenu<br />
+				selector: <em>farm-context-menu</em><br />
+				<span style="color: green;">ready for use</span>
+				`,
+			},
+		},
+		viewMode: 'docs',
 	},
 };
 
 export const Primary = () => ({
-	template: `<farm-context-menu :items="[{ label: 'Remover' }]" />`,
+	template: `<div>
+		<farm-context-menu :items="[{ label: 'Remover', icon: { color: 'error', type: 'open-in-new' } }]" />
+	</div>`,
 });
 
 export const Icons = () => ({
-	template: `<farm-context-menu
-		:items="[{ label: 'Remover', icon: { color: 'error', type: 'delete' } }]" />`,
+	template: `<div>
+		<farm-context-menu
+			ref="icons"
+			:items="[{ label: 'Remover', icon: { color: 'error', type: 'delete' } }]"
+		/>
+	</div>`,
 });
 
 export const Multi = () => ({
@@ -32,5 +47,7 @@ export const Multi = () => ({
 			],
 		};
 	},
-	template: `<farm-context-menu :items="items" />`,
+	template: `<div>
+		<farm-context-menu ref="multi" :items="items" />
+	</div>`,
 });
