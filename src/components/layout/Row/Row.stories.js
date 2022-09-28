7f26a555864a448744fa8f9ce02c31235399d480
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-09-28 21:33:34 +0100

    feat: STOR-97

--- src/components/layout/Row/Row.stories.js
+++ src/components/layout/Row/Row.stories.js
@@ -20,4 +20,49 @@ export const Primary = () => ({
 	template: '<farm-row>row content</farm-row>',
 });
 
-Primary.storyName = 'Basic';
+export const Align = () => ({
+	data() {
+		return {
+			aligns: ['start', 'center', 'end', 'baseline', 'stretch'],
+		};
+	},
+	template: `<div>
+		<farm-row :align="align" v-for="align in aligns" :key="'align_' + align">
+			<v-col md="6">
+				row align
+				{{ align }}
+			</v-col>
+		</farm-row>
+	</div>`,
+});
+
+export const AlignContent = () => ({
+	data() {
+		return {
+			aligns: ['start', 'center', 'end', 'space-between', 'space-around', 'stretch'],
+		};
+	},
+	template: `<div>
+		<farm-row :align-content="align" v-for="align in aligns" :key="'align-content-_' + align">
+			<v-col md="6">
+				row align-content
+				{{ align }}
+			</v-col>
+		</farm-row>
+	</div>`,
+});
+
+export const Justify = () => ({
+	data() {
+		return {
+			justifications: ['start', 'center', 'end', 'space-between', 'space-around'],
+		};
+	},
+	template: `<div>
+		<farm-row :justify="k" v-for="k in justifications" :key="'justify-_' + k">
+			<v-col md="6">
+				row justify {{ k }}
+			</v-col>
+		</farm-row>
+	</div>`,
+});
