6a51b7cce3d87afdfd4749b2d546e805d4db1c32
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-09-30 20:32:21 +0100

    feat: STOR-85 STOR-86

--- src/components/DataTablePaginator/DataTablePaginator.stories.js
+++ src/components/DataTablePaginator/DataTablePaginator.stories.js
@@ -9,7 +9,8 @@ export default {
 		docs: {
 			description: {
 				component: `Paginator with items per page select<br />
-				selector: <em>farm-datatable-paginator</em>
+				selector: <em>farm-datatable-paginator</em><br />
+				<span style="color: var(--farm-primary-base);">ready for use</span>
 				`,
 			},
 		},
@@ -22,10 +23,9 @@ export default {
 };
 
 export const Primary = () => ({
-	components: { DataTablePaginator },
 	data() {
 		return {
-			totalPages: 8
+			totalPages: 8,
 		};
 	},
 	template: '<DataTablePaginator :totalPages="totalPages" :page="1" />',
@@ -33,46 +33,18 @@ export const Primary = () => ({
 		setTimeout(() => {
 			this.totalPages = 0;
 		}, 1000);
-	}
+	},
 });
 
 export const Secondary = () => ({
-	components: { DataTablePaginator },
 	template: '<DataTablePaginator :hidePerPageOptions="true" :totalPages="190000" :page="1" />',
 });
 
 export const Disabled = () => ({
-	components: { DataTablePaginator },
 	template: '<DataTablePaginator :disabled="true":totalPages="19" :page="1" />',
 });
 
 export const CustomPerPage = () => ({
-	components: { DataTablePaginator },
 	template:
 		'<DataTablePaginator :perPageOptions="[1, 2, 4, 12, 27]" :totalPages="19" :page="1" />',
 });
-
-const parameters = {
-	design: {
-		type: 'figma',
-		url: 'https://www.figma.com/file/1f84J4m1IBghWhozQvdyyt/%E2%9C%8D---Design-System?node-id=1503%3A921',
-	},
-};
-
-Primary.story = {
-	name: 'Básico',
-	parameters,
-};
-Secondary.story = {
-	name: 'Sem limite por páginas',
-	parameters,
-};
-Disabled.story = {
-	name: 'Desabilitado',
-	parameters,
-};
-CustomPerPage.story = {
-	name: 'Lista de registros por página customizada',
-	parameters,
-};
-
