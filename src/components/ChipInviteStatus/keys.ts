b54a01b487be46337a41584ec3d79091fb78d376
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-10-07 19:19:18 +0100

    feat: STOR-139

---
+++ src/components/ChipInviteStatus/keys.ts
@@ -0,0 +1,11 @@
+export default {
+	10: { label: 'Convidar', color: 'primary' },
+	11: { label: 'Convidado', color: 'warning', outlined: true },
+	12: { label: 'Incompleto', color: 'warning', outlined: true },
+	13: { label: 'Concluído', color: 'primary', outlined: true },
+	14: { label: 'Falha/Erro', color: 'error', outlined: true },
+	15: { label: 'Em Análise', color: 'info', outlined: true },
+	16: { label: 'Em Andamento', color: 'secondary', variation: 'darken' },
+	17: { label: 'Em Espera', color: 'secondary' },
+	18: { label: 'Declinado', color: 'error', outlined: true },
+};
