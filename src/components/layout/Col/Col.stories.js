3cdbfcb594a6c5fc29e867d251bb0ff674d4b387
Victor Marques <vmarques.ext@farminvestimentos.com.br>
2022-10-10 16:58:58 -0300

    documentação row e col

--- src/components/layout/Col/Col.stories.js
+++ src/components/layout/Col/Col.stories.js
@@ -20,22 +20,100 @@ export const Primary = () => ({
 	template: '<farm-col>col</farm-col>',
 });
 export const Xl = () => ({
-	template: '<farm-col xl="3">xl: 3</farm-col>',
+	template: `<farm-row>
+		<farm-col xl="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+		<farm-col xl="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+		<farm-col xl="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+		<farm-col xl="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+		<farm-col xl="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+		<farm-col xl="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col xl="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+	</farm-row>`,
 });
 export const Lg = () => ({
-	template: '<farm-col lg="3">lg: 3</farm-col>',
+	template: `<farm-row>
+	<farm-col lg="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+	<farm-col lg="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+	<farm-col lg="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+	<farm-col lg="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+	<farm-col lg="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+	<farm-col lg="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col lg="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+</farm-row>`,
 });
 export const Md = () => ({
-	template: '<farm-col md="3">md: 3</farm-col>',
+	template: `<farm-row>
+	<farm-col md="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+	<farm-col md="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+	<farm-col md="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+	<farm-col md="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+	<farm-col md="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+	<farm-col md="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col md="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+</farm-row>`,
 });
 export const Sm = () => ({
-	template: '<farm-col sm="4">sm: 4</farm-col>',
+	template: `<farm-row>
+	<farm-col sm="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+	<farm-col sm="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+	<farm-col sm="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+	<farm-col sm="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+	<farm-col sm="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+	<farm-col sm="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col sm="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+</farm-row>`,
 });
 export const SmVcol = () => ({
-	template: '<v-col sm="4" style="border:1px solid red">sm: 4</v-col>',
+	template: `<farm-row>
+	<farm-col smvcol="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+	<farm-col smvcol="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+	<farm-col smvcol="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+	<farm-col smvcol="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+	<farm-col smvcol="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+	<farm-col smvcol="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col smvcol="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+</farm-row>`,
 });
 export const Xs = () => ({
-	template: '<farm-col xs="4">xs: 4</farm-col>',
+	template: `<farm-row>
+	<farm-col xs="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 3</farm-col>
+	<farm-col xs="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 3</farm-col>
+	<farm-col xs="4" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 3</farm-col>
+	<farm-col xs="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 2</farm-col>
+	<farm-col xs="6" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 2</farm-col>
+	<farm-col xs="12" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 1</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>1 of 6</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>2 of 6</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>3 of 6</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>4 of 6</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>5 of 6</farm-col>
+		<farm-col xs="2" style='background-color: rgba(112.520718,44.062154,249.437846, 0.1); border: 1px solid rgba(112.520718,44.062154,249.437846, 0.25)'>6 of 6</farm-col>
+</farm-row>`,
 });
 export const Combination = () => ({
 	template: '<farm-col xl="3" xs="4">xl: 3 - xs: 4</farm-col>',
