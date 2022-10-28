b46b0a3ff19f7cd338de8d481a9ae60ad6b5df4e
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-10-28 12:55:58 +0100

    refactor: avoid redundant imports

--- src/components/CopyToClipboard/CopyToClipboard.stories.js
+++ src/components/CopyToClipboard/CopyToClipboard.stories.js
@@ -19,28 +19,24 @@ export default {
 };
 
 export const Primary = () => ({
-	components: { 'farm-copytoclipboard': CopyToClipboard },
 	template: `<div style="max-width: 480px; padding-top: 80px; padding-left: 80px;">
 		<farm-copytoclipboard toCopy="To be copied" />
     </div>`,
 });
 
 export const NoIcon = () => ({
-	components: { 'farm-copytoclipboard': CopyToClipboard },
 	template: `<div style="max-width: 480px; padding-top: 80px; padding-left: 80px;">
 		<farm-copytoclipboard toCopy="To be copied" :isIcon="false" />
     </div>`,
 });
 
 export const CustomSuccessMessage = () => ({
-	components: { 'farm-copytoclipboard': CopyToClipboard },
 	template: `<div style="max-width: 480px; padding-top: 80px; padding-left: 80px;">
 		<farm-copytoclipboard toCopy="To be copied" success-message="Custom Success Message" />
     </div>`,
 });
 
 export const CustomTooltipColor = () => ({
-	components: { 'farm-copytoclipboard': CopyToClipboard },
 	template: `<div style="max-width: 480px; padding-top: 80px; padding-left: 80px;">
 		<farm-copytoclipboard toCopy="To be copied" tooltip-color="info" />
     </div>`,
