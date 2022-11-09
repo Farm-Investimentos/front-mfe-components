80ebaec630acef893ad63dd4599aeb8919e2d0f2
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-11-09 17:13:07 +0000

    feat: better css and example for deprecated textfield STOR-76-77-187

--- src/components/TextField/TextField.stories.js
+++ src/components/TextField/TextField.stories.js
@@ -9,7 +9,8 @@ export default {
 		docs: {
 			description: {
 				component: `Text field<br />
-				selector: <em>farm-texfield</em>
+				selector: <em>farm-texfield</em><br />
+				<span style="color: var(--farm-primary-base);">ready for use</span>
 				`,
 			},
 		},
@@ -24,12 +25,34 @@ export default {
 export const Primary = () => ({
 	data() {
 		return {
-			v: '',
+			v: 'input text',
 		};
 	},
 	template: `<div style="width: 480px">
 		<farm-textfield v-model="v" />
 		value: {{ v }}
+	</div>`,
+});
+
+export const Disabled = () => ({
+	data() {
+		return {
+			v: 'disabled field',
+		};
+	},
+	template: `<div style="width: 480px">
+		<farm-textfield v-model="v" disabled />
+	</div>`,
+});
+
+export const Readonly = () => ({
+	data() {
+		return {
+			v: 'readonly field',
+		};
+	},
+	template: `<div style="width: 480px">
+		<farm-textfield v-model="v" readonly />
 	</div>`,
 });
 
@@ -45,18 +68,37 @@ export const BindVar = () => ({
 	</div>`,
 });
 
-export const Rules = () => ({
+export const Validate = () => ({
 	data() {
 		return {
-			v: '',
+			v1: 'input 1',
+			v2: '',
+			v3: '',
+			v4: '',
 			rules: {
-				required: val => !!val,
+				required: value => !!value || 'Required field',
+				email: v =>
+					!v ||
+					/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+					'Must be an e-mail',
 			},
 		};
 	},
 	template: `<div style="width: 480px">
-		<h4>Custom rule (required field)</h4>
-		<farm-textfield v-model="v" :rules="[rules.required]" />
-		value: {{ v }}
+		<farm-label required>Required field</farm-label>
+		<farm-textfield v-model="v1" :rules="[rules.required]" />
+
+		<farm-label>E-mail</farm-label>
+		<farm-textfield v-model="v2" :rules="[rules.email]" />
+
+		<farm-label required>Required and e-mail</farm-label>
+		<farm-textfield v-model="v3" :rules="[rules.required, rules.email]" />
+
+		<farm-label required>Required field with hint</farm-label>
+		<farm-textfield v-model="v4" :rules="[rules.required]" hint="hint text" />
+
+		<farm-label required>Required field with icon</farm-label>
+		<farm-textfield v-model="v1" :rules="[rules.required]" append-icon="mdi-eye" />
+
 	</div>`,
 });
