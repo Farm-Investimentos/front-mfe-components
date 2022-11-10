17eb52072987cea8eb65a8e6075435c464b61e25
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-11-10 17:38:21 +0000

    feat: STOR-104 105

--- src/components/Select/Select.stories.js
+++ src/components/Select/Select.stories.js
@@ -25,6 +25,23 @@ export default {
 export const Primary = () => ({
 	data() {
 		return {
+			v: null,
+			items: [
+				{ value: 1, text: ' value 1' },
+				{ value: 2, text: ' value 2' },
+				{ value: 3, text: ' value 3' },
+			],
+		};
+	},
+	template: `<div style="width: 480px">
+		<farm-select v-model="v" :items="items" />
+		v-model: {{ v }}
+	</div>`,
+});
+
+export const InitialValue = () => ({
+	data() {
+		return {
 			v: 1,
 			items: [
 				{ value: 1, text: ' value 1' },
@@ -70,5 +87,56 @@ export const Disabled = () => ({
 	template: `<div style="width: 480px">
 		<farm-select v-model="v" :items="items" disabled />
 		v-model: {{ v }}
+	</div>`,
+});
+
+export const Validate = () => ({
+	data() {
+		return {
+			v: null,
+			items: [
+				{ value: null, text: '' },
+				{ value: 1, text: ' value 1' },
+				{ value: 2, text: ' value 2' },
+				{ value: 3, text: ' value 3' },
+			],
+			rules: {
+				required: value => !!value || 'Required field',
+			},
+		};
+	},
+	template: `<div style="width: 480px">
+		<farm-label required>Required</farm-label>
+		<farm-select v-model="v" :items="items" :rules="[rules.required]" />
+		v-model: {{ v }}
+	</div>`,
+});
+
+
+export const Reset = () => ({
+	data() {
+		return {
+			v: 1,
+			items: [
+				{ value: 1, text: ' value 1' },
+				{ value: 2, text: ' value 2' },
+				{ value: 3, text: ' value 3' },
+			],
+			rules: {
+				required: value => !!value || 'Required field',
+			},
+		};
+	},
+	methods: {
+		click() {
+			this.$refs.select.reset();
+		}
+	},
+	template: `<div style="width: 480px">
+		<farm-select v-model="v" :items="items" ref="select" :rules="[rules.required]" />
+		v-model: {{ v }}
+		<farm-btn @click="click">
+			reset
+		</farm-btn>
 	</div>`,
 });
