7456478f5ced95b23be33f777349ddcbd83ab927
Chris Benseler - FARM <84783765+christian-benseler-farm@users.noreply.github.com>
2022-11-22 10:17:56 -0300

    feat: STOR-299 (#735)
    * feat: STOR-299
    
    * feat: refactor scss STOR-299
    
    * feat: refactor scss STOR-299

--- src/components/RadioGroup/RadioGroup.vue
+++ src/components/RadioGroup/RadioGroup.vue
@@ -2,104 +2,94 @@
 	<div
 		:class="{
 			'farm-radio-group': true,
-			'farm-radio-group--vertical': $props.vertical,
 		}"
-		:color="color"
 	>
-		<div
-			class="farm-radio-group__item"
-			v-for="(button, index) in buttons"
-			:key="`farm-radio-group_` + index"
-			@click="clicked(button.id)"
-		>
-			<input
-				type="radio"
-				name="radio"
-				:checked="button.id === innerValue"
-				:id="`farm-radio-group_` + index"
-				:value="button.id"
-				:class="{ checked: button.id === innerValue }"
-			/>
-			<label> {{ button.label }} </label>
-		</div>
+		<slot></slot>
+		<farm-caption v-if="showErrorText" color="error" variation="regular">
+			{{ errorBucket[0] }}
+		</farm-caption>
 	</div>
 </template>
 <script lang="ts">
-import Vue, { PropType, ref, watch } from 'vue';
+import Vue, { ref, watch, PropType, toRefs, computed, onBeforeMount } from 'vue';
 import validateFormStateBuilder from '../../composition/validateFormStateBuilder';
-import IRadioGroup from './IRadioGroup';
+import validateFormFieldBuilder from '../../composition/validateFormFieldBuilder';
+import validateFormMethodBuilder from '../../composition/validateFormMethodBuilder';
+import deepEqual from '../../composition/deepEqual';
+
 export default Vue.extend({
 	name: 'farm-radio-group',
 	props: {
-		/**
-		 * Array of buttons
-		 */
-		buttons: {
-			type: Array as PropType<Array<IRadioGroup>>,
-			default: [],
-		},
 		/**
 		 * v-model
 		 */
 		value: {
 			required: true,
 		},
+		errorMessage: String,
 		/**
-		 * Is vertical?
+		 * Array of rules used for validation
 		 */
-		vertical: {
-			type: Boolean,
-			default: false,
+		rules: {
+			type: Array as PropType<Array<Function>>,
+			default: () => [],
 		},
-		color: {
-			type: String as PropType<
-				| 'primary'
-				| 'secondary'
-				| 'neutral'
-				| 'info'
-				| 'success'
-				| 'error'
-				| 'warning'
-				| 'success'
-				| 'extra-1'
-				| 'extra-2'
-				| 'gray'
-			>,
-			default: 'primary',
-		}
 	},
 	setup(props, { emit }) {
+		const { rules } = toRefs(props);
 		const innerValue = ref(props.value);
 		const { errorBucket, valid, validatable } = validateFormStateBuilder();
+		const isTouched = ref(false);
 
-		const reset = () => {
-			innerValue.value = false;
-			emit('input', innerValue.value);
-		};
+		let fieldValidator = validateFormFieldBuilder(rules.value);
+
+		const hasError = computed(() => {
+			return errorBucket.value.length > 0;
+		});
+
+		const showErrorText = computed(() => hasError.value && isTouched.value);
 
 		watch(
 			() => props.value,
 			() => {
+				isTouched.value = true;
 				innerValue.value = props.value;
+				validate(innerValue.value);
 			}
 		);
 
-		const clicked = value => {
-			innerValue.value = value;
+		watch(
+			() => props.rules,
+			(newVal, oldVal) => {
+				if (deepEqual(newVal, oldVal)) return;
+				fieldValidator = validateFormFieldBuilder(rules.value);
+				validate = validateFormMethodBuilder(errorBucket, valid, fieldValidator);
+				validate(innerValue.value);
+			}
+		);
+
+		let validate = validateFormMethodBuilder(errorBucket, valid, fieldValidator);
+
+		const reset = () => {
+			innerValue.value = null;
 			emit('input', innerValue.value);
 		};
+
+		onBeforeMount(() => {
+			validate(innerValue.value);
+		});
 
 		return {
 			innerValue,
 			errorBucket,
 			valid,
 			validatable,
+			hasError,
+			showErrorText,
+			isTouched,
 			reset,
-			clicked,
+			validate,
 		};
 	},
 });
 </script>
-<style lang="scss" scoped>
-@import './RadioGroup.scss';
-</style>
