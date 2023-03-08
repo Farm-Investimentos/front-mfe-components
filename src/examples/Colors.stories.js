069630c342aa53beabb13701607cf577f17801ca
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2023-03-08 13:30:54 +0000

    docs: STOR-505

--- src/examples/Colors.stories.js
+++ src/examples/Colors.stories.js
@@ -1,6 +1,8 @@
 import baseThemeColors from '../configurations/_theme-colors-base.scss';
 import textThemeColors from '../configurations/_theme-colors-text.scss';
 import strokeThemeColors from '../configurations/_theme-colors-stroke.scss';
+import bwThemeColors from '../configurations/_theme-colors-bw.scss';
+import backgroundThemeColors from '../configurations/_theme-colors-background.scss';
 import variationThemeColors from '../configurations/_theme-colors-variations.scss';
 import('./Colors.stories.scss');
 
@@ -16,6 +18,10 @@ export const NamesAndValues = () => ({
 			variationThemeColors: { ...variationThemeColors },
 			textThemeColors: { ...textThemeColors },
 			strokeThemeColors: { ...strokeThemeColors },
+			bwThemeColorsKeys: Object.keys(bwThemeColors),
+			bwThemeColors: { ...bwThemeColors },
+			backgroundThemeColorsKeys: Object.keys(backgroundThemeColors),
+			backgroundThemeColors: { ...backgroundThemeColors },
 		};
 	},
 	template: `<div class="palette-container">
@@ -40,7 +46,21 @@ export const NamesAndValues = () => ({
 			<span :style="{ color: strokeThemeColors['stroke-base'] }">Base <span>{{ strokeThemeColors['stroke-base'] }}</span></span>
 			<span :style="{ color: strokeThemeColors['stroke-disabled'] }">Disabled <span>{{ strokeThemeColors['stroke-disabled'] }}</span></span>
 			<span :style="{ color: strokeThemeColors['stroke-divider'] }">Divider <span>{{ strokeThemeColors['stroke-divider'] }}</span></span>
-        </div>
+		</div>
+
+		<div>
+			<farm-heading :type="5">Black & White</farm-heading>
+			<span v-for="bw in bwThemeColorsKeys" :key="'bw_name_' + bw" :style="{ color: bw !== 'white' ? bwThemeColors[bw] : 'black' }">
+				{{ bw }}<span>{{ bwThemeColors[bw] }}</span>
+			</span>
+		</div>
+
+		<div>
+			<farm-heading :type="5">Background</farm-heading>
+			<span v-for="bw in backgroundThemeColorsKeys" :key="'bw_name_' + bw" :style="{ backgroundColor: backgroundThemeColors[bw] }">
+				{{ bw }}<span>{{ backgroundThemeColors[bw] }}</span>
+			</span>
+		</div>
 
 	</div>`,
 });
@@ -53,6 +73,10 @@ export const CssVariables = () => ({
 			variationThemeColors: { ...variationThemeColors },
 			textThemeColors: { ...textThemeColors },
 			strokeThemeColors: { ...strokeThemeColors },
+			bwThemeColorsKeys: Object.keys(bwThemeColors),
+			bwThemeColors: { ...bwThemeColors },
+			backgroundThemeColorsKeys: Object.keys(backgroundThemeColors),
+			backgroundThemeColors: { ...backgroundThemeColors },
 		};
 	},
 	template: `<div class="palette-container">
@@ -79,6 +103,20 @@ export const CssVariables = () => ({
 			<span :style="{ color: strokeThemeColors['stroke-disabled'] }">Disabled <span>--farm-stroke-disabled</span></span>
 			<span :style="{ color: strokeThemeColors['stroke-divider'] }">Divider <span>--farm-stroke-divider</span></span>
         </div>
+
+		<div>
+			<farm-heading :type="5">Black & White</farm-heading>
+			<span v-for="bw in bwThemeColorsKeys" :key="'bw_var_' + bw" :style="{ color: bw !== 'white' ? bwThemeColors[bw] : 'black' }">
+				{{ bw }} <span>--farm-bw-{{ bw }}</span>
+			</span>
+		</div>
+
+		<div>
+			<farm-heading :type="5">Background</farm-heading>
+			<span v-for="bw in backgroundThemeColorsKeys" :key="'bw_var_' + bw" :style="{ backgroundColor: backgroundThemeColors[bw], }">
+				{{ bw }} <span>--farm-background-{{ bw }}</span>
+			</span>
+		</div>
 
 	</div>`,
 });
