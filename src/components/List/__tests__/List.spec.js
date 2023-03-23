4dc6381edc8ad30fb4ca804dd4ff24acf42bfe1e
Dirceu Rezende <dirceu.rezende@farminvestimentos.com.br>
2023-03-23 16:27:38 -0300

    test: add new unit tests.

--- src/components/List/__tests__/List.spec.js
+++ src/components/List/__tests__/List.spec.js
@@ -3,9 +3,11 @@ import List from '../List';
 
 describe('List component', () => {
 	let wrapper;
+	let component;
 
 	beforeEach(() => {
 		wrapper = shallowMount(List);
+		component = wrapper.vm;
 	});
 
 	test('Created hook', () => {
@@ -15,6 +17,17 @@ describe('List component', () => {
 	describe('mount component', () => {
 		it('renders correctly', () => {
 			expect(wrapper.element).toMatchSnapshot();
+		});
+	});
+	describe('Methods', () => {
+		it('should isFocused be true when call onFocusin', () => {
+			component.onFocusin();
+			expect(component.isFocused).toBeTruthy();
+		});
+
+		it('should isFocused be false when call onFocusout', () => {
+			component.onFocusout();
+			expect(component.isFocused).toBeFalsy();
 		});
 	});
 });
