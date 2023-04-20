7ed044198895e5df763e6f82e18a4037545554d6
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2023-04-20 11:13:55 +0100

    test: update coverage settings

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
 	setupFiles: ['<rootDir>/tests/unit/index.js'],
 	modulePaths: ['<rootDir>', 'node_modules'],
-	collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js', '!src/**/*.stories.js'],
+	moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx,vue}', '!src/main.js', '!src/**/*.stories.{js,jsx,ts,tsx,vue}'],
 	coveragePathIgnorePatterns: ['src/examples'],
 	transformIgnorePatterns: ['<rootDir>/node_modules/(?!vuetify)'],
 };
