9aba06b8e38af9333d23d6499f0778505fe27fd7
Kainan Gabriel <55860684+KainanGB@users.noreply.github.com>
2023-04-20 15:21:10 -0300

    feat: STOR-549 (#1065)
    * feat: added two new props to resourcemetainfo component
    
    * refact: added spec

--- src/components/ResourceMetaInfo/__tests__/ResourceMetaInfo.spec.js
+++ src/components/ResourceMetaInfo/__tests__/ResourceMetaInfo.spec.js
@@ -10,9 +10,11 @@ describe('ResourceMetaInfo component', () => {
 			propsData: {
 				infos: {
 					createdAt: '21/10/2022',
+					createdBy: 'Test User',
 					updatedAt: '21/10/2022',
 					username: 'Test User',
 					updatedHours: '10:10:10',
+					createdHours: '10:10:10',
 				},
 			},
 		});
@@ -73,6 +75,32 @@ describe('ResourceMetaInfo component', () => {
 			});
 		});
 
+		describe('showCreateBy', () => {
+			it('should return true when it has an createby date', () => {
+				expect(component.showCreatedBy).toBeTruthy();
+			});
+
+			it('should return false when it has not an createdby date', async () => {
+				await wrapper.setProps({
+					infos: { createdBy: null },
+				});
+				expect(component.showCreatedBy).toBeFalsy();
+			});
+		});
+
+		describe('showCreatedHours', () => {
+			it('should return true when it has an createdHours', () => {
+				expect(component.showCreatedHours).toBeTruthy();
+			});
+
+			it('should return false when it has not an createdHours', async () => {
+				await wrapper.setProps({
+					infos: { createdHours: null },
+				});
+				expect(component.showCreatedHours).toBeFalsy();
+			});
+		});
+
 		describe('formattedCreatedAt', () => {
 			it('should return infos createdAt', () => {
 				expect(component.formattedCreatedAt).toBe('21/10/2022');
@@ -83,6 +111,19 @@ describe('ResourceMetaInfo component', () => {
 					infos: { createAt: null },
 				});
 				expect(component.formattedCreatedAt).toBe('N/A');
+			});
+		});
+
+		describe('formattedCreatedBy', () => {
+			it('should return infos createdBy', () => {
+				expect(component.formattedCreatedBy).toBe('Test User');
+			});
+
+			it('should return N/A when infos createdBy is empty', async () => {
+				await wrapper.setProps({
+					infos: { createdBy: null },
+				});
+				expect(component.formattedCreatedBy).toBe('N/A');
 			});
 		});
 
@@ -122,6 +163,19 @@ describe('ResourceMetaInfo component', () => {
 					infos: { updatedHours: null },
 				});
 				expect(component.formattedUpdatedHours).toBe('N/A');
+			});
+		});
+
+		describe('formattedCreatedHours', () => {
+			it('should return infos createdHours', () => {
+				expect(component.formattedCreatedHours).toBe('10:10:10');
+			});
+
+			it('should return N/A when infos createdHours is empty', async () => {
+				await wrapper.setProps({
+					infos: { createdHours: null },
+				});
+				expect(component.formattedCreatedHours).toBe('N/A');
 			});
 		});
 	});
