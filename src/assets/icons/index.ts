e409ac9751b162e87853090f3e6d2a734d96d715
Sadraque Santos <sdq.sts@gmail.com>
2025-05-30 17:08:11 -0300

    feat: Add custom icons support and enhance Icon component functionality

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+// Custom Icons exports
+// Centralized export for all custom icons functionality
+
+export { default as customIconsList, customIconsList as customIcons } from './custom-icons-list';
+
+// Re-export individual items for convenience
+export * from './custom-icons-list';
